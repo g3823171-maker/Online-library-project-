5c188e97c68ac2fb8427b8cdd3f7c81b9a97df14
g3823171-maker <g3823171@gmail.com>
2026-02-04 20:59:36 +0530

    a1.php

---
+++ a1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>login test</title>
+</head>
+<body>
+<form name="login" id="login" method="POST">
+<label>username</label>
+<input type="text" name="user_name" id="login" value="" placeholder="give the username">
+<label>password</label>
+<input type="text" name="password" id="login" placeholder="give the username"> 
+<input type="submit" name="submit" id="login"> 
+
+
+
+
+</form></body>
+</html>
